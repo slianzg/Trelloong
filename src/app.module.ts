80e7d0656a54ef7939aff0332629e4c75aa5d276
cocojerry1 <ymchoi526@gmail.com>
2024-03-18 16:47:11 +0900

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ColumnModule } from './column/column.module';
 
 @Module({
-  imports: [],
+  imports: [ColumnModule],
   controllers: [AppController],
   providers: [AppService],
 })
