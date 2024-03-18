2d185651d123c8556915582722987d7fadc67d43
김성진 <jinni1226@naver.com>
2024-03-18 23:34:22 +0900

    Feat/댓글엔티티추가

--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,35 @@
-export class Comment {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Card } from 'src/card/entities/card.entity';
+import { Member } from 'src/member/entities/member.entity';
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'comments' })
+export class Comment {
+    @PrimaryGeneratedColumn()
+    commentId: number;
+
+    @IsNumber()
+    @Column('varchar', { nullable: false })
+    memberId: number;
+
+    @IsString()
+    @Column('date', { nullable: false })
+    @IsNotEmpty({ message: '내용을 입력하세요.' })
+    commentContent: string;
+
+    @IsNumber()
+    @Column('int', { nullable: false })
+    cardId: number;
+
+    @CreateDateColumn()
+    @Column('date', { nullable: false })
+    createdAt: Date;
+
+    // @ManyToOne(() => Member, (member) => member.comments)
+    // @JoinColumn({ name: "memberId", referencedColumnName: "memberId" })
+    // member: Member;
+
+    // @ManyToOne(() => Card, (card) => card.comments)
+    // @JoinColumn({ name: "cardId", referencedColumnName: "cardId" })
+    // card: Card;
+}
