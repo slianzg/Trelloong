5bab79346059e108d80c02f9d158cbed1b009764
slianzg <slian201248@gmail.com>
2024-03-18 20:36:10 +0900

    Refactor: 초기파일 업데이트

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,62 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { MemberModule } from './member/member.module';
+import { BoardModule } from './board/board.module';
+import { ColumnModule } from './column/column.module';
+import { CardModule } from './card/card.module';
+import { CommentModule } from './comment/comment.module';
+import { GroupModule } from './group/group.module';
+import Joi from 'joi';
+import { Member } from './member/entities/member.entity';
+import { Board } from './board/entities/board.entity';
+import { Column } from './column/entities/column.entity';
+import { Card } from './card/entities/card.entity';
+import { Group } from './group/entities/group.entity';
+import { Comment } from './comment/entities/comment.entity';
+
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mysql',
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    database: configService.get('DB_NAME'),
+    entities: [Member, Board, Column, Card, Comment, Group],
+    synchronize: configService.get('DB_SYNC'),
+    logging: true,
+  }),
+  inject: [ConfigService],
+};
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET_KEY: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNC: Joi.boolean().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    MemberModule,
+    BoardModule,
+    ColumnModule,
+    CardModule,
+    CommentModule,
+    GroupModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
