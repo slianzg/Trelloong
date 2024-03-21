080b78757a1f47d0c9ca23ff96d3410a5c9992e3
김성진 <jinni1226@naver.com>
2024-03-21 16:12:17 +0900

    feat/card.entity

--- src/card/entities/card.entity.ts
+++ src/card/entities/card.entity.ts
@@ -1,1 +1,29 @@
-export class Card {}
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity({
+  name: 'cards',
+})
+export class Card {
+  @PrimaryGeneratedColumn()
+  cardId: number;
+  @Column({ type: 'varchar', nullable: false })
+  cardName: string;
+  @Column({ type: 'text', nullable: true })
+  cardDescription: string;
+  @Column({ type: 'date', nullable: true })
+  dueDate: Date;
+  setDueDate(date: Date) {
+    this.dueDate = date;
+  }
+  @Column({ type: 'bigint', default: 1 })
+  cardOrder: number;
+  @BeforeInsert()
+  increaseOrder() {
+    this.cardOrder++;
+  }
+  @Column({ type: 'varchar', nullable: true })
+  cardColor: string;
+  @Column({ type: 'json', nullable: true })
+  assignedTo: any[];
+  @Column()
+  columnId: number;
+}
