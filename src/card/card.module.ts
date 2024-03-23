18613b0170a25873ef2d41ba068747cced5aeb6c
tkddnd010 <tkddnd010@naver.com>
2024-03-23 17:17:00 +0900

    bugfix: 서버 실행 하도록 버그 수정

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { MemberModule } from 'src/member/member.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Card } from './entities/card.entity';
 
 @Module({
-  imports: [MemberModule],
+  imports: [TypeOrmModule.forFeature([Card]), MemberModule],
   controllers: [CardController],
   providers: [CardService],
 })
