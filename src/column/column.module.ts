8cd789542208a16f7027c99a7d283fd5d5e88153
slianzg <slian201248@gmail.com>
2024-03-23 17:33:32 +0900

    Fix: 자잘한 오타 수정& 에러 처리 추가

--- src/column/column.module.ts
+++ src/column/column.module.ts
@@ -4,12 +4,9 @@ import { ColumnsService } from './column.service';
 import { Columns } from './entities/column.entity';
 import { Board } from 'src/board/entities/board.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardService } from 'src/board/board.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Board, Columns]),
-  ],
+  imports: [TypeOrmModule.forFeature([Board, Columns])],
   providers: [ColumnsService],
   controllers: [ColumnsController],
   exports: [ColumnsService],
