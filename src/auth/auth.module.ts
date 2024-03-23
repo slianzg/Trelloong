7478523627e12a4505af071ab8852f54970406ed
slianzg <slian201248@gmail.com>
2024-03-24 00:18:26 +0900

    Fix: MemberGuard 오류 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { UserModule } from 'src/user/user.module';
+import { MemberModule } from 'src/member/member.module';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     UserModule,
+    MemberModule,
   ],
   providers: [JwtStrategy],
 })
--- src/auth/member.guard.ts
+++ src/auth/member.guard.ts
@@ -2,10 +2,14 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { MemberService } from 'src/member/member.service';
 import { Role } from 'src/types/role.type';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class MemberGuard extends AuthGuard('jwt') implements CanActivate {
-  constructor(private readonly memberService: MemberService) {
+  constructor(
+    private readonly memberService: MemberService,
+    private readonly userService: UserService,
+  ) {
     super();
   }
 
@@ -20,8 +24,7 @@ export class MemberGuard extends AuthGuard('jwt') implements CanActivate {
     const { user } = context.switchToHttp().getRequest();
 
     let boardId = request.params.boardId;
-
-    const member = await this.memberService.findMember(boardId, user.userId);
+    const member = await this.memberService.findMember(+boardId, user.userId);
 
     if (!member || member.role === Role.User) {
       return false;
